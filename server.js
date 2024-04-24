8bd521df92fd9f6cd2c2d55cc7bfbc816f22a9e9
NandhiniKarvendhan <nandhukannan10@gmail.com>
2024-04-24 17:42:54 +0530

    Updated

--- server.js
+++ server.js
@@ -10,17 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://Cluster98117:37akbBZb4LFbUosP@cluster98117.pteb4fk.mongodb.net/Event')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB', err);
-  });
+mongoose.connect('mongodb+srv://Cluster98117:37akbBZb4LFbUosP@cluster98117.pteb4fk.mongodb.net/Event', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});
 
 // Define a schema for your data
 const registrationSchema = new mongoose.Schema({
-  name: String,
+  name: String,  
   email: String,
   phoneNumber: String,
   password: String
@@ -31,26 +32,27 @@ const Registration = mongoose.model('Registration', registrationSchema);
 
 // Handle form submission
 app.post('/', (req, res) => {
-  const { name, email, phoneNumber, password } = req.body;
-  console.log(req.body);
+  const { name, email, phoneNumber, password} = req.body;
+console.log(req.body);
   // Create a new registration document
   const newRegistration = new Registration({
     name, email, phoneNumber, password
   });
 
   // Save the document to the database
-  newRegistration.save()
-    .then(() => {
-      console.log('Form data saved successfully');
-      // Redirect to the success page after successful registration
-      res.redirect('/success.html');
-    })
-    .catch(err => {
-      console.error('Error saving form data', err);
-      res.status(500).send('Error saving form data');
-    });
+  newRegistration.save().then(() => {
+    console.log('Form data saved successfully');
+    
+     // Redirect to the success page after successful registration
+     res.redirect('/success.html');
+  }).catch(err => {
+    console.error('Error saving form data', err);
+    res.status(500).send('Error saving form data');
+  });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
